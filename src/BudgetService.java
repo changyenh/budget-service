d93c162c74c8311183d9a14d866a943d1e38d86f
changyen.huang <changyen.huang@linecorp.com>
2020-10-27 18:25:09 +0800

    simplify

--- src/BudgetService.java
+++ src/BudgetService.java
@@ -1,6 +1,4 @@
-import java.time.Duration;
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -10,29 +8,32 @@ import java.util.stream.IntStream;
 
 public class BudgetService {
 
-    private final IBudgetRepo iBudgetRepo;
-    private final Map<String, Double> dailyBudgetMap;
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+  private final IBudgetRepo iBudgetRepo;
+  private final Map<String, Double> budgetPerDay;
+  private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
 
 
-    public BudgetService(IBudgetRepo iBudgetRepo) {
-        this.iBudgetRepo = iBudgetRepo;
-        this.dailyBudgetMap = this.iBudgetRepo.getAll()
-                .stream()
-                .collect(
-                        Collectors.toMap(
-                                b -> b.yearMonth,
-                                b -> ((b.amount * 1.0) / YearMonth.parse(b.yearMonth, formatter).getMonth().maxLength())));
+  public BudgetService(IBudgetRepo iBudgetRepo) {
+    this.iBudgetRepo = iBudgetRepo;
+    this.budgetPerDay = this.iBudgetRepo.getAll()
+        .stream()
+        .collect(Collectors.toMap(
+            b -> b.yearMonth,
+            b -> ((b.amount * 1.0) / parseMonthLength(b.yearMonth))));
+  }
 
-    }
+  private int parseMonthLength(String yearMonth) {
+    return YearMonth.parse(yearMonth, formatter).getMonth().maxLength();
+  }
 
 
-    public double query(final LocalDate startTime, final LocalDate endTime) {
-        int days = (int)ChronoUnit.DAYS.between(startTime, endTime) + 1;
-        return IntStream.range(0, days).boxed().mapToDouble(i -> {
-            LocalDate date = startTime.plusDays(i);
-            return dailyBudgetMap.getOrDefault(formatter.format(date), 0.0);
-        }).sum();
-    }
+  public double query(final LocalDate startTime, final LocalDate endTime) {
+    int days = (int) ChronoUnit.DAYS.between(startTime, endTime) + 1;
+    return IntStream.range(0, days).boxed().mapToDouble(i -> getBudget(startTime.plusDays(i))).sum();
+  }
+
+  private double getBudget(LocalDate date) {
+    return budgetPerDay.getOrDefault(formatter.format(date), 0.0);
+  }
 
 }
